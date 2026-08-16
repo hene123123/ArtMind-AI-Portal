b4ce32afd16b292f8decd72bc8730bc2ed11f8ed
noteblockMC <noteblock818@gmail.com>
2026-08-16 20:41:54 +0700

    git commit update minh-components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppRoutes from './routes/AppRoutes';
+import AIChatWidget from './components/AIChatWidget'
+
+function App() {
+  return (
+    <>
+      <AppRoutes />
+      <AIChatWidget />
+    </>
+  );
+}
+
+export default App;
